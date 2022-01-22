5c85fe0a67ac7afd23515ff2d1362d576794c572
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 16:40:05 +0100

    update

--- magic_number.py
+++ magic_number.py
@@ -1,5 +1,16 @@
+MIN_NUMBER = 1
+MAX_NUMBER = 10
+MAX_TRIES = 3
+
+
 def main():
     pass
+
+
+def intro():
+    print("=" * 50, "MAGIC NUMBER", "=" * 50)
+    print(f"There is a number between {MIN_NUMBER} and {MAX_NUMBER}. Can you guess it?")
+    print(f"You have {MAX_TRIES} tries.")
 
 
 main()
