f7e669130d37d8cd6801ebbc1b45a68e868c8506
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 14:31:46 +0100

    update

---
+++ 03_multiple_parameters.py
@@ -0,0 +1,8 @@
+# define a function / declare a function
+# positional arguments
+def say_hello(name, address, email):
+    print(f"Hello {name}. Your address is: {address}. Yout email is: {email}")
+
+
+# call a function
+say_hello("Robert", "Budapest", "mail.pythonsuli@gmail.com")
