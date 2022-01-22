4820ebf19988ddc992119a78d69173b2b8fd1875
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 15:12:20 +0100

    update

---
+++ 07_random_length_keyword_params.py
@@ -0,0 +1,9 @@
+def random_length_keyword_params(**kwargs):
+    print(kwargs)
+
+
+random_length_keyword_params(
+    first_name="Robert",
+    last_name="Vari",
+    email="mail.pythonsuli@gmail.com"
+)
