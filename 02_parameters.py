c8402aa87f30d14a72dee97bc64670b0fb74cafc
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 14:23:07 +0100

    update

---
+++ 02_parameters.py
@@ -0,0 +1,10 @@
+# define a function / declare a function
+def say_hello(name):
+    print(f"Hello {name}")
+
+
+# call a function
+say_hello("Robert")
+say_hello("Csaba")
+say_hello("Kriszta")
+say_hello("Csilla")
