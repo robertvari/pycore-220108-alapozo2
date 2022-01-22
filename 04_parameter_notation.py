55106665bbd045d00b67c12fe09a387227b91eb9
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 14:36:45 +0100

    update

---
+++ 04_parameter_notation.py
@@ -0,0 +1,6 @@
+# parameter notation param_name: type
+def say_hello(name: str, address: str, email: str):
+    print(f"Hello {name}. Your address is: {address}. Yout email is: {email}")
+
+
+say_hello("Robert", "Budapest", "mail.pythonsuli@gmail.com")
