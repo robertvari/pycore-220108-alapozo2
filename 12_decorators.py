6a354392d91efc6868d73e5eecb532bc9fa344db
Robert Vári <mail.robertvari@gmail.com>
2022-01-22 18:00:12 +0100

    update

--- 12_decorators.py
+++ 12_decorators.py
@@ -1,8 +1,35 @@
 import random, time
 
 
-def worker():
+def worker1():
+    print("Worker1 Started")
     time.sleep(random.randint(3, 10))
+    print("Worker1 finished!")
 
 
-worker()
+def worker2():
+    print("Worker2 Started")
+    time.sleep(random.randint(3, 10))
+    print("Worker2 finished!")
+
+
+def worker3():
+    print("Worker3 Started")
+    time.sleep(random.randint(3, 10))
+    print("Worker3 finished!")
+
+
+start_time = time.time()
+worker1()
+
+print(f"Job time: {time.time()-start_time}")
+
+start_time = time.time()
+worker2()
+
+print(f"Job time: {time.time()-start_time}")
+
+start_time = time.time()
+worker3()
+
+print(f"Job time: {time.time()-start_time}")
