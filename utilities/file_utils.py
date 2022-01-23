12a74726cc61096b27e96d71dd2fb51965913c21
Robert Vári <mail.robertvari@gmail.com>
2022-01-23 15:11:45 +0100

    update

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -5,7 +5,21 @@ def get_files(root_folder: str, files=[], name_filter=None):
     #            condition = True/False,         Message to user
     assert os.path.exists(root_folder), f"Folder does not exist {root_folder}"
 
-    print("Get all files...")
+    # files and folders in root_folder
+    folder_content = os.listdir(root_folder)
 
+    # get all files skip folders
+    for i in folder_content:
+        if os.path.isfile(os.path.join(root_folder, i)):
+            files.append(os.path.join(root_folder, i))
 
-get_files(r"C:\Work\_PythonSuli\pycore-220108")
+    # todo get subfolders
+    subfolders = []
+
+    for i in folder_content:
+        if os.path.isdir(os.path.join(root_folder, i)):
+            subfolders.append(os.path.join(root_folder, i))
+
+    pass
+
+get_files(r"C:\Work\_PythonSuli\pycore-220108\alapozo2")
