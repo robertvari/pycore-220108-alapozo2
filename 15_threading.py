8d5d7b5e7ca451b70c0836e2c543de94e4fadf93
Robert Vári <mail.robertvari@gmail.com>
2022-01-23 16:29:09 +0100

    update

--- 15_threading.py
+++ 15_threading.py
@@ -1,9 +1,20 @@
-import random, time
+import random, time, threading
 
 
-def worker():
-    print("Worker Started")
+def worker1():
+    print("Worker1 Started")
     time.sleep(random.randint(3, 10))
-    print("Worker finished!")
+    print("Worker1 finished!")
 
-worker()
+
+def worker2():
+    print("Worker2 Started")
+    time.sleep(random.randint(3, 10))
+    print("Worker2 finished!")
+
+
+t1 = threading.Thread(target=worker1)
+t2 = threading.Thread(target=worker2)
+
+t1.start()
+t2.start()
