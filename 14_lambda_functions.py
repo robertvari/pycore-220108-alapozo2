1a945f154de364d8c0806c7a89983838392633ab
Robert Vári <mail.robertvari@gmail.com>
2022-01-23 15:56:53 +0100

    update

--- 14_lambda_functions.py
+++ 14_lambda_functions.py
@@ -9,3 +9,13 @@ print(divide_numbers(10, 2))
 name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna", "Kiss Elemér Aladár"]
 print(sorted(name_list))
 print(sorted(name_list, key=lambda name: name.split()[-1]))
+
+students = {
+    "0": {"name": "Vári Róbert", "grade": 45},
+    "1": {"name": "Kiss Elemér", "grade": 32},
+    "3": {"name": "Nagy Adrienn", "grade": 56},
+    "4": {"name": "Tóth Barna", "grade": 12},
+    "5": {"name": "Kiss Elemér Aladár", "grade": 86},
+}
+
+print(sorted(students, key=lambda id: students[id]["grade"]))
