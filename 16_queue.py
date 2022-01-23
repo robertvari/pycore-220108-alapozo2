4b49400f6664fe95fada9492e9eb6e9859c4e75e
Robert Vári <mail.robertvari@gmail.com>
2022-01-23 16:49:28 +0100

    update

--- 16_queue.py
+++ 16_queue.py
@@ -10,6 +10,14 @@ files = get_files(r"C:\Work\_PythonSuli\pycore-220108\photos")
 for i in files:
     job_queue.put(i)
 
+
 def file_worker():
-    while True:
-        pass
+    while not job_queue.empty():
+        next_job = job_queue.get()
+        print(f"{threading.current_thread().name}: I'm working on {next_job}")
+
+        time.sleep(random.randint(1, 10))
+
+        print(f"{threading.current_thread().name}: I finsihed {next_job}!!!")
+
+        job_queue.task_done()
